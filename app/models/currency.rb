9c8fe19e5e026441489e5157136e127451e7dbf2
jrhe1213 <jiarong1213@gmail.com>
2021-02-10 22:48:34 -0500

    first commit

---
+++ app/models/currency.rb
@@ -0,0 +1,23 @@
+class Currency < ApplicationRecord
+
+  def current_price
+    url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion'
+    query = { 
+      "symbol"     => self.currency_symbol,
+      "amount"      => 1,
+    }
+    headers = { 
+      "X-CMC_PRO_API_KEY"  => "a96f0fc3-8807-4166-8736-297930edbbe8",
+    }
+    request = HTTParty.get(
+      url, 
+      :query => query,
+      :headers => headers
+    )
+    response = JSON.parse(request.body)['data'][0]['quote']['USD']['price'] || '0'
+  end
+
+  def calculate_value(amount)
+    (current_price.to_f * amount.to_f).round(4)
+  end
+end
